3fd4eef262a311d2669b53071498750611d18ba8
nikolina-czart <nikola.czart@gmail.com>
2023-01-19 13:24:42 +0100

    Init commit

---
+++ src/main/java/com/example/PDTestServer/PdTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PDTestServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdTestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PDTestServer/PdTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PDTestServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
